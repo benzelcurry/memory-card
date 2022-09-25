2ab6ee937cee1199d93b7de9c87918c4a975767f
Ben <benawhiting@gmail.com>
2022-09-25 12:46:14 -0700

    Create basic styling for webpage

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import '../stylesheets/Cards.css';
+
+const Cards = () => {
+  return (
+    <div className='body'>
+      Cards Go Here...
+    </div>
+  )
+}
+
+export default Cards;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import '../stylesheets/Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      Hello, World!
+    </div>
+  )
+}
+
+export default Header;
