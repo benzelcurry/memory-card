8163a4fc4d3d2289f87fb71beff6e5fb142a94aa
Ben <benawhiting@gmail.com>
2022-09-25 13:35:06 -0700

    Style cards section

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,8 +3,21 @@ import '../stylesheets/Cards.css';
 
 const Cards = () => {
   return (
-    <div className='body'>
-      Cards Go Here...
+    <div className="body-container">
+      <div className='body'>
+        <div className="card">Card 1</div>
+        <div className="card">Card 2</div>
+        <div className="card">Card 3</div>
+        <div className="card">Card 4</div>
+        <div className="card">Card 5</div>
+        <div className="card">Card 6</div>
+        <div className="card">Card 7</div>
+        <div className="card">Card 8</div>
+        <div className="card">Card 9</div>
+        <div className="card">Card 10</div>
+        <div className="card">Card 11</div>
+        <div className="card">Card 12</div>
+      </div>
     </div>
   )
 }
