cddbb93bf58bf7cf74abf275545ce136f5bd61b0
Ben <benawhiting@gmail.com>
2022-09-26 14:11:31 -0700

    Make deck shuffle after each click

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import '../stylesheets/Cards.css';
 
 const Cards = () => {
   let [score, setScore] = useState(0);
   let [best, setBest] = useState(0);
-  let [cardArr, setCardArr] = useState([{}]);
+  let [cardArr, setCardArr] = useState([
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12
+  ]);
   let [cards, setCards] = useState({
     1: false,
     2: false,
@@ -46,7 +59,21 @@ const Cards = () => {
     if ((best === 0 || best <= score) && stopBest === false) {
       setBest(best + 1);
     }
+
+    shuffle(cardArr)
   }
+
+  // Shuffles an array of 1-12 and then returns each index to
+  // variables to be used in the JSX
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      [array[i], array[j]] = [array[j], array[i]]
+    }
+  }
+
+
 
   return (
     <div className="body-container">
@@ -54,24 +81,20 @@ const Cards = () => {
         <div className="current">Current: {score}</div>
         <div className="best">Best: {best}</div>
       </div>
-      {cardArr.map(gamecards => {
-        return (
-          <div className='body'>
-            <div className="card" onClick={handleClick} id='1'>Card 1</div>
-            <div className="card" onClick={handleClick} id='2'>Card 2</div>
-            <div className="card" onClick={handleClick} id='3'>Card 3</div>
-            <div className="card" onClick={handleClick} id='4'>Card 4</div>
-            <div className="card" onClick={handleClick} id='5'>Card 5</div>
-            <div className="card" onClick={handleClick} id='6'>Card 6</div>
-            <div className="card" onClick={handleClick} id='7'>Card 7</div>
-            <div className="card" onClick={handleClick} id='8'>Card 8</div>
-            <div className="card" onClick={handleClick} id='9'>Card 9</div>
-            <div className="card" onClick={handleClick} id='10'>Card 10</div>
-            <div className="card" onClick={handleClick} id='11'>Card 11</div>
-            <div className="card" onClick={handleClick} id='12'>Card 12</div>
+      <div className='body'>
+            <div className="card" onClick={handleClick} id={cardArr[0]}>Card {cardArr[0]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[1]}>Card {cardArr[1]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[2]}>Card {cardArr[2]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[3]}>Card {cardArr[3]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[4]}>Card {cardArr[4]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[5]}>Card {cardArr[5]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[6]}>Card {cardArr[6]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[7]}>Card {cardArr[7]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[8]}>Card {cardArr[8]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[9]}>Card {cardArr[9]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[10]}>Card {cardArr[10]}</div>
+            <div className="card" onClick={handleClick} id={cardArr[11]}>Card {cardArr[11]}</div>
           </div>
-        )
-      })}
     </div>
   )
 }
